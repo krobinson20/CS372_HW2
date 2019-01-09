7a3d347f8a8fbfc9fc0cf9f9d3437e99c085ebf7
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 10:55:25 -0800

    feedback

--- CityHall.java
+++ CityHall.java
@@ -1,7 +1,10 @@
+//PT -- javadoc. -2
 public class CityHall extends Building{
 public CityHall(){
     occupants[0] = new Kid();
     occupants[1] = new Teacher();
     occupants[3] = new Police(); 
 }
+    
+    //Need to be able to add/remove occupants
 }
