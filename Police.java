7a710a9a55838b5c86612f17476c3091000f9403
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 10:48:47 -0800

    feedback

--- Police.java
+++ Police.java
@@ -1,6 +1,7 @@
 public class Police extends Person implements Employee{
     public enum PoliceRole{Patrol, Sargent, Captain, Cheif};
     public PoliceRole Role;
+    //PT -- javadoc throughout. -2
     public Police()
     {
         Role = PoliceRole.Patrol;
@@ -12,9 +13,11 @@ public class Police extends Person implements Employee{
         Name = _name;
         Role = _role;
     }
+    //You should be keeping track of how much they are paid. -1
     public boolean PayEmployee(Employee e){
         return true;
     }
+    //PT -- ??? -2
     public boolean GetID(Employee e){
         return true;
     }
